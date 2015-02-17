003d4d26b4ee99d035f2e89167c14937dfec7689
jens-peterolsson <jens-peter.olsson@it-budet.se>
2015-02-18 00:56:55 +0100

    Initial commit

---
+++ OfficeSouls/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace OfficeSouls
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ OfficeSouls/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OfficeSouls.MvcApplication" Language="C#" %>
