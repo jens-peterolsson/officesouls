9d620b51aa00b19eb124d4ccf2fb881d6d7a0a74
jens-peterolsson <jens-peter.olsson@it-budet.se>
2015-03-25 21:35:00 +0100

    Basic domain classes

---
+++ Itbudet.OfficeSouls.Domain.Tests/SoulTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Itbudet.OfficeSouls.Domain.Tests
+{
+    [TestClass]
+    public class SoulTests
+    {
+        [TestMethod]
+        public void AgeBasic()
+        {
+            var age = new Soul {Born = new DateTime(DateTime.Now.Year - 2, 1, 1)}.Age;
+            Assert.AreEqual(2, age);
+        }
+
+        [TestMethod]
+        public void AgeBeforeBirthday()
+        {
+            // don't run this test on new year's eve...
+            var age = new Soul { Born = new DateTime(DateTime.Now.Year - 2, 12, 31) }.Age;
+            Assert.AreEqual(1, age);
+        }
+    }
+}
---
+++ Itbudet.OfficeSouls.Domain/Mood.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itbudet.OfficeSouls.Domain
+{
+    public class Mood
+    {
+        public int Now { get; set; }
+        public int Today { get; set; }
+        public int Week { get; set; }
+        public int Month { get; set; }
+        public int Season { get; set; }
+        public int Year { get; set; }
+        public int LastFiveYears { get; set; }
+        public int Eternity { get; set; }
+    }
+}
