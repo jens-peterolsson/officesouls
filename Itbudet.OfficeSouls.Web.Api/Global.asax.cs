452e6bfc1fac5d6e1c16fbd97870ab3205bfe1b9
jens-peterolsson <jens-peter.olsson@it-budet.se>
2015-03-26 00:25:05 +0100

    Stripped down web api project to just the necessary stuff

--- Itbudet.OfficeSouls.Web.Api/Global.asax.cs
+++ Itbudet.OfficeSouls.Web.Api/Global.asax.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Mvc;
-using System.Web.Optimization;
+﻿using System.Web.Http;
 using System.Web.Routing;
 
 namespace Itbudet.OfficeSouls.Web.Api
@@ -13,11 +7,7 @@ namespace Itbudet.OfficeSouls.Web.Api
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
