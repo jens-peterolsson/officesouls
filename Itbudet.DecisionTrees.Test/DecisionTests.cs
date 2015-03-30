1c894df5bc8a8e74533e2bb344515d730e9a1784
jens-peterolsson <jens-peter.olsson@it-budet.se>
2015-03-31 00:42:10 +0200

    1st working version of decision tree

---
+++ Itbudet.DecisionTrees.Test/DecisionTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Itbudet.DecisionTrees.Test
+{
+    [TestClass]
+    public class DecisionTests
+    {
+        private Decision _tested;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _tested = new Decision();
+        }
+
+        [TestMethod]
+        public void TestBasicValues()
+        {
+            Assert.AreEqual(2, _tested.Decide(10, 2));
+            Assert.AreEqual(0, _tested.Decide(5, 1));
+            Assert.AreEqual(1, _tested.Decide(5, 2));
+        }
+    }
+}
--- Itbudet.DecisionTrees/Decision.cs
+++ Itbudet.DecisionTrees/Decision.cs
@@ -1,4 +1,5 @@
-﻿using Accord.MachineLearning.DecisionTrees;
+﻿using System.Linq;
+using Accord.MachineLearning.DecisionTrees;
 using Accord.MachineLearning.DecisionTrees.Learning;
 using Accord.Math;
 using AForge;
@@ -7,19 +8,27 @@ namespace Itbudet.DecisionTrees
 {
     public class Decision
     {
-        public int Decide(int happy, int hoursOfSleep)
+        public int Decide(int hoursOfSleep, int happy)
         {
+            // no of hours of sleep / happy / result, tired = 0. otherwise 1
+            double[][] learningData =
+            {
+                new double[] { 6, 0, 1 }, new double[] { 9, 1, 1 }, new double[] { 4, 2, 1 },
+                new double[] { 9, 2, 2 }, new double[] { 8, 1, 1 }, new double[] { 7, 0, 0 },
+                new double[] { 6, 1, 1 }, new double[] { 5, 1, 0 }, new double[] { 8, 2, 2 },
+                new double[] { 5, 2, 1 }, new double[] { 4, 1, 0 }, new double[] { 6, 0, 0 }
+            };
+
             // Creates a matrix from the entire source data table
-            double[,] table = null;// = (dgvLearningSource.DataSource as DataTable).ToMatrix(out columnNames);
+            double[,] table = learningData.ToMatrix();
 
-            // Get only the input vector values (first two columns)
-            double[][] inputs = null;// = table.GetColumns(0, 1).ToArray();
-
+            var inputs = learningData.Select(i => new [] {i[0], i[1]}).ToArray();
+            // first array = no of hours sleep, second array = happy
             // Get only the output labels (last column)
-            int[] outputs = null;// = table.GetColumn(2).ToInt32();
+            var outputs = learningData.Select(i => (int) i[2]).ToArray();
 
             // Specify the input variables
-            var rangeSleep = new DoubleRange(1, 10);
+            var rangeSleep = new DoubleRange(0, 20);
             var rangeHappy = new DoubleRange(0, 2);
 
             DecisionVariable[] variables = 
@@ -28,7 +37,7 @@ namespace Itbudet.DecisionTrees
                 DecisionVariable.Continuous("y", rangeHappy)
             };
 
-            var tree = new DecisionTree(variables, 2);
+            var tree = new DecisionTree(variables, 3);
 
             // Create the C4.5 learning algorithm
             var c45 = new C45Learning(tree);
@@ -36,20 +45,21 @@ namespace Itbudet.DecisionTrees
             // Learn the decision tree using C4.5
             double error = c45.Run(inputs, outputs);
 
+            //TODO: create the winforms project to see spread?
             // Get the ranges for each variable (X and Y)
-            DoubleRange[] ranges = Matrix.Range(table, 0);
+            //var ranges = table.Range(0);
 
-            // Generate a Cartesian coordinate system
-            double[][] map = Matrix.CartesianProduct(
-                Matrix.Interval(ranges[0], 0.05),
-                Matrix.Interval(ranges[1], 0.05));
+            //// Generate a Cartesian coordinate system
+            //double[][] map = Matrix.CartesianProduct(
+            //    Matrix.Interval(ranges[0], 0.05),
+            //    Matrix.Interval(ranges[1], 0.05));
 
             // Classify each point in the Cartesian coordinate system
-            double[] result = map.Apply(tree.Compute).ToDouble();
+            //double[] result = map.Apply(tree.Compute).ToDouble();
             //double[,] surface = map.ToMatrix().InsertColumn(result);
 
-            //var z = tree.Compute(new int[] {7, 1});
-            return 0;
+            var result = tree.Compute(new int[] {hoursOfSleep, happy});
+            return result;
         }
     }
 }
